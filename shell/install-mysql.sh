efe63f7f53a33fb8d63e90115988210f691283cb
Charlie Goddard <charlie@iamcharliegoddard.com>
2015-05-26 22:24:08 -0700

    updated mysql install for CentOS

--- shell/install-mysql.sh
+++ shell/install-mysql.sh
@@ -7,26 +7,23 @@ CODENAME=$(/bin/bash /vagrant/shell/os-detect.sh CODENAME)
 sudo su
 
 if [ "$OS" == 'debian' ] || [ "$OS" == 'ubuntu' ]; then
+    echo "Ubuntu/Debian mode detected. Installing MySQL 5.6"
+    add-apt-repository -y ppa:ondrej/mysql-5.6
+    apt-get update
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+    apt-get install -y mysql-server-5.6
 
-	echo "Ubuntu/Debian mode detected. Installing MySQL 5.6"
-	add-apt-repository -y ppa:ondrej/mysql-5.6
-	apt-get update
-	debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-	debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-	apt-get install -y mysql-server-5.6
+    # https://github.com/fideloper/Vaprobash/blob/master/scripts/mysql.sh
+    echo "Commenting out bind-address to make MySQL accept remote connections."
+    sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf
+
+    echo "Adding a root user for remote connections"
+    cat /vagrant/shell/install-mysql.sql | mysql -u root -proot
+    service mysql restart
 
 elif [[ "$OS" == 'centos' ]]; then
-
-	echo "CentOS installation not defined yet"
-	# @Todo
-
+    yum -y install mysql-server mysql
+    chkconfig --levels 235 mysqld on
+    service mysqld start
 fi
-
-echo "Commenting out bind-address to make MySQL accept remote connections."
-sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf
-
-echo "Adding a root user for remote connections"
-cat /vagrant/shell/install-mysql.sql | mysql -u root -proot
-service mysql restart
-
-# Above code partially grabbed from https://github.com/fideloper/Vaprobash/blob/master/scripts/mysql.sh
